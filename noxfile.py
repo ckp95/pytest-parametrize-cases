2376f3f84b9aef1c7cd988a63bb6f97bacc58bab
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2020-12-12 18:16:45 +0000

    add nox test for whether py.typed is included

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,6 @@
+import tempfile
+from pathlib import Path
+
 import nox
 
 locations = ["src", "tests", "noxfile.py"]
@@ -14,6 +17,26 @@ def mypy(session):
     args = session.posargs or ["src"]
     session.run("poetry", "install", external=True)
     session.run("mypy", "--strict", *args)
+
+
+@nox.session(python=["3.8", "3.9"])
+def py_typed(session):
+    # have to pip install to make sure not in editable mode, see
+    # https://github.com/python-poetry/poetry/issues/1382
+    session.run("pip", "install", "-q", ".", "mypy")
+
+    # if we haven't included py.typed in the package, mypy --strict should
+    # complain about missing imports. if we have included it, mypy is happy.
+    script = "from pytest_parametrize_cases import Case, parametrize_cases"
+    # script = "def foo() -> int: return 5"
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        filename = "foo.py"
+        filepath = Path(temp_dir) / filename
+        with open(filepath, "w") as f:
+            print(script, file=f)
+
+        session.run("mypy", "--strict", str(filepath))
 
 
 @nox.session(python=["3.8", "3.9"])
